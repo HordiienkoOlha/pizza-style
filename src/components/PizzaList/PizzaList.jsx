3e0cfdcf517cb45b695f9710569e47bc9baad017
HordiienkoOlha <olhahordiienkomail@gmail.com>
2023-04-28 13:49:57 +0300

    update PizzaList.jsx

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -4,28 +4,62 @@ import {
   Card,
   CardContent,
   CardMedia,
-  IconButton,
   List,
   ListItem,
   Typography,
+  styled,
 } from "@mui/material";
 import RemoveCircleOutlineTwoToneIcon from "@mui/icons-material/RemoveCircleOutlineTwoTone";
 import AddCircleOutlineTwoToneIcon from "@mui/icons-material/AddCircleOutlineTwoTone";
 
 import styles from "./PizzaList.module.css";
-import { useState } from "react";
+import {
+  // useEffect,
+  useState,
+} from "react";
+
+const ReverseButton = styled(Button)`
+  &:hover {
+    background-color: rgba(74, 20, 140, 0.69);
+    color: #fff;
+  }
+`;
 
 const PizzaList = ({ products }) => {
-  const [cartProduct, setCardProduct] = useState(1)
-  console.log(products);
+  const [cartProduct, setCardProduct] = useState(null);
+  // const [showAddButton, setShowAddButton] = useState(true);
+  // console.log(products);
+  console.log(cartProduct);
+
+  const addProduct = () => setCardProduct(cartProduct + 1);
+  const removeProduct = () => setCardProduct(cartProduct - 1);
+
+  // useEffect(() => {
+  //   // if (!cartProduct) {
+  //   //   return;
+  //   // }
+  //   if (cartProduct?.length > 1) {
+  //     console.log(cartProduct);
+  //     setShowAddButton(false);
+  //   }
+
+  //   return;
+  // }, [cartProduct]);
+
   return (
     <>
       <List className={styles.list}>
         {products &&
           products.map(({ id, title, description, price, image }) => (
-            <ListItem key={id}>
-              <Card sx={{ height: "100%" }}>
-                <CardMedia component="img" image={image} alt={title} />
+            <ListItem key={id} className={styles.item}>
+              <Card sx={{ height: "100%" }} className={styles.card}>
+                <CardContent>
+                  <div className={styles.cardImageContainer}>
+
+
+                <CardMedia component="img" image={image} alt={title} className={styles.cardImage}/>
+                  </div>
+                </CardContent>
 
                 <CardContent>
                   <Typography variant="h6" component="h2" gutterBottom>
@@ -34,21 +68,38 @@ const PizzaList = ({ products }) => {
                   <Typography variant="body2" sx={{ minHeight: "60px" }}>
                     {description}
                   </Typography>
-                  <Typography variant="h5" component="h3" color="secondary">
+                  <Typography
+                    variant="h5"
+                    component="h3"
+                    color="secondary"
+                    sx={{ p: 1 }}
+                  >
                     Price: {price}
                   </Typography>
                 </CardContent>
                 <CardContent className={styles.buttonWrapper}>
-                  <Button variant="outlined">Add to cart</Button>
-                  <IconButton>
-                    <RemoveCircleOutlineTwoToneIcon />
-                  </IconButton>
-                  <Typography variant="h6" display="flex" alignItems="center">
-                  {cartProduct}
-                  </Typography>
-                  <IconButton>
-                    <AddCircleOutlineTwoToneIcon />
-                  </IconButton>
+                  {/* {cartProduct?.length > 1 ? ( */}
+                    <>
+                      <ReverseButton onClick={removeProduct}>
+                        <RemoveCircleOutlineTwoToneIcon />
+                      </ReverseButton>
+                      <Typography
+                        variant="h6"
+                        display="flex"
+                      alignItems="center"
+                      sx={{p:2}}
+                      >
+                        {cartProduct}
+                      </Typography>
+                      <ReverseButton onClick={addProduct}>
+                        <AddCircleOutlineTwoToneIcon />
+                      </ReverseButton>
+                    </>
+                  {/* ) : ( */}
+                    <ReverseButton variant="outlined" onClick={addProduct}>
+                      Add to cart
+                    </ReverseButton>
+                  {/* )} */}
                 </CardContent>
               </Card>
             </ListItem>
